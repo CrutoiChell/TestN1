9a864b881040154cf241b4342b250fac36206d87
CrutoiChell <144133892+CrutoiChell@users.noreply.github.com>
2024-09-10 18:00:37 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [arr, setArr] = useState([1, 2, 3, 4, 5])
+  const [list, setList] = useState('')
+
+  function add() {
+    if (list) {
+      setArr([...arr, list])
+      setList('')
+    }
+  }
+
+  let result = arr.map((item, index) => {
+    return <li key={index}>{item}</li>
+  })
+
+  return (
+    <>
+    <h1>{list}</h1>
+      <ul>
+        {result}
+      </ul>
+      <input value={list} onChange={(event) => setList(event.target.value)} type="text" />
+      <button onClick={add}>click</button>
+    </>
+  )
+}
